4531841fb3da6c813aa76c59bfd06af21aee2010
moon_ <m3252@naver.com>
2020-11-08 15:28:02 +0900

    JPA exam 학습

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,42 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMEBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
