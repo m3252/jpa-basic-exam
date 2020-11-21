c73641ffabdba8c4811017141e336e05115afd7f
moon_ <m3252@naver.com>
2020-11-21 19:47:05 +0900

    임베디드 타입과 값 타입 안정성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -15,17 +17,41 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Address address = new Address("city", "Street", "zipcode");
-
             Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(address);
-            member.setWorkPeriod(new Period());
+            member.setUsername("1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
 
-            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("===============================");
+            Member findMember = em.find(Member.class, member.getId());
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+//            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+
+//            List<Address> addressHistory = findMember.getAddressHistory();
+//            for(Address address : addressHistory)
+//                System.out.println("address = " + address.getCity() );
+//
+//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+//            for(String favoriteFood : favoriteFoods)
+//                System.out.println("favoriteFood = " + favoriteFood);
+
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,6 +2,10 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 @Entity
@@ -15,21 +19,16 @@ public class Member{
     private String username;
 
     @Embedded
-    private Period workPeriod;
-
-    @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name ="city",
-                    column = @Column(name = "WORK_CITY")),
-            @AttributeOverride(name ="street",
-                    column = @Column(name = "WORK_STREET")),
-            @AttributeOverride(name ="zipcode",
-                    column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address officeAddress;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name="MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -47,20 +46,29 @@ public class Member{
         this.username = username;
     }
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
 
